0fb96e269ca41bacca24936c2be983067e759964
Trevor Frewin <tjfrewin@gmail.com>
2025-07-31 13:50:32 +0100

    Leveraging the new Parallel.For()

--- tfrewin.play.fractal.start/processor/JuliaSetProcessor.cs
+++ tfrewin.play.fractal.start/processor/JuliaSetProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using tfrewin.play.fractal.start.processor.output;
 
 namespace tfrewin.play.fractal.start.processor
@@ -9,15 +10,16 @@ namespace tfrewin.play.fractal.start.processor
         {
             const double cX = -0.7;
             const double cY = 0.27015;
-            double zx, zy, tmp;
-            int i;
 
             var returnThis = new Matrix(maximumIteration);
 
-            for (int x = 0; x < planeWidth; x++)
+            Parallel.For(0, planeWidth, (x) =>
             {
-                for (int y = 0; y < planeHeight; y++)
+                Parallel.For(0, planeHeight, (y) =>
                 {
+                    double zx, zy, tmp;
+                    int i;
+
                     zx = 1.5 * (x - planeWidth / 2) / (0.5 * zoom * planeWidth) + moveX;
                     zy = 1.0 * (y - planeHeight / 2) / (0.5 * zoom * planeHeight) + moveY;
 
@@ -76,8 +78,8 @@ namespace tfrewin.play.fractal.start.processor
                     }
 
                     returnThis.Add(new Point(x, y, i));
-                }
-            }
+                });
+            });
 
             return returnThis;
         }
